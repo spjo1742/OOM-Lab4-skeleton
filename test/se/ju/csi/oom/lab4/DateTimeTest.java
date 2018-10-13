0e3f299de46d83e0ad9bc7d66c579797b63633d7
Josef <spjo1742@student.ju.se>
2018-10-13 13:38:45 +0200

    Update test/se/ju/csi/oom/lab4/DateTimeTest.java

---
+++ test/se/ju/csi/oom/lab4/DateTimeTest.java
@@ -0,0 +1,26 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		DateTime birth = new DateTime(1985, 4, 21, 7, 13, 37);
+		assertEquals(birth.toString(), "1985-04-21 21:13:37");
+	}
+
+	@Test
+	public void testDateTimeString() {
+		DateTime birth = new DateTime("1985-04-21 21:13:37");
+		assertEquals(birth.toString(), "1985-04-21 21:13:37");
+	}
+
+}
