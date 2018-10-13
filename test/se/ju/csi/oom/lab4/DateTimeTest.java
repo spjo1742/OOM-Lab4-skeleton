259017d3e0326d7e1252e5a86618677ec94ccf2b
Josef <spjo1742@student.ju.se>
2018-10-13 14:15:47 +0200

    Update test/se/ju/csi/oom/lab4/DateTimeTest.java

---
+++ test/se/ju/csi/oom/lab4/DateTimeTest.java
@@ -0,0 +1,24 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDateTimeString() {
+		fail("Not yet implemented");
+	}
+
+}
